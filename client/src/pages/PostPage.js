cd10469a4cec2f39ef5d0197782fc15476cbfc9c
Piotr Zalejski <piotrzalejski@outlook.com>
2024-02-28 19:58:04 -0600

    feat: Add API call to fetch post data in PostPage.js

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -1,4 +1,20 @@
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+
 export default function PostPage() {
+  const params = useParams();
+
+  useEffect(() => {
+    async function getPost() {
+      const res = await fetch(
+        `${process.env.REACT_APP_API_URL}/posts/${params.id}`
+      );
+      const data = await res.json();
+      console.log(data);
+    }
+    getPost();
+  }, []);
+
   return (
     <div>
       <h1>Post Page</h1>
