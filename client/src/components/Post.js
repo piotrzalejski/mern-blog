b7f2aec1b150a70478b54230482e45025da20578
Piotr Zalejski <piotrzalejski@outlook.com>
2024-04-10 21:39:38 -0500

    Refactor post image styling

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -8,14 +8,12 @@ export default function Post({ id, title, summary, image, createdAt, author }) {
         <time>{format(new Date(createdAt), 'MMM dd, yyyy')}</time>
         <p className='post-author'>{author}</p>
       </header>
-      <div className='post'>
-        <div className='post-img'>
-          <img src={image} alt='Blog banner' />
-        </div>
-        <div className='post-info'>
-          <h2 className='post-title'>{title}</h2>
-          <p className='post-summary'>{summary}</p>
-        </div>
+      <div className='post-img'>
+        <img className='post--imgage' src={image} alt='Blog banner' />
+      </div>
+      <div className='post-info'>
+        <h2 className='post-title'>{title}</h2>
+        <p className='post-summary'>{summary}</p>
       </div>
     </Link>
   );
